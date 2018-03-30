0163c8c8ca8e421c5dfcd46bba8cb7d881606600
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2018-03-29 23:01:21 -0500

    config

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    appID: '12f845628f3ba1563b5a6646b41b80b1d6ca2ad5ed57a953231e18b937cb81b1',
+    appSecret: '13449f604d8019a479eb8b9500d89ec37a83ef58253a4c338bf4f3870d02e57b',
+    // timeAPI: '0c485ad4ffc3435b9aa11650171310',
+    timeAPI: 'd8a8fac804da469bbf8175052182503',
+
+    //amdoren: 'qer6be5kLvhm5ci5mfWP9cUzdYQV46',
+    google: 'AIzaSyAggKUYNCN63nyuJITBqI8usRcH_GDwzck'
+}
