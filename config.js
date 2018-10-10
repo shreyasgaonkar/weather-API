9f35f232d49873e8b2ca02798205857d2c650733
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2018-10-09 23:16:02 -0500

    removed config

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    appID: '12f845628f3ba1563b5a6646b41b80b1d6ca2ad5ed57a953231e18b937cb81b1',
-    appSecret: '13449f604d8019a479eb8b9500d89ec37a83ef58253a4c338bf4f3870d02e57b',
-    // timeAPI: '0c485ad4ffc3435b9aa11650171310',
-    timeAPI: 'd8a8fac804da469bbf8175052182503',
-    loc: '0f53f67b6dbf330908f89139646c8978',
-    //amdoren: 'qer6be5kLvhm5ci5mfWP9cUzdYQV46',
-    google: 'AIzaSyAggKUYNCN63nyuJITBqI8usRcH_GDwzck'
-}
